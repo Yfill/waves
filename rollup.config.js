3b6957a3c6bae21ab29ab73545e42ee4d2bac480
Yfill <vps998@gmail.com>
2021-01-10 19:36:15 +0800

    chore: add build support for esm and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -31,15 +31,40 @@ const developmentPlugins = [
 const productionPlugins = [
   terser(),
 ];
+const name = 'Waves';
+const banner = `/*! ${pkg.name} v${pkg.version} | ${new Date().toGMTString()} | ${pkg.homepage} */`;
+const proOutputOpt = {
+  name,
+  banner,
+  exports: 'default',
+  sourcemap: false,
+};
 export default {
   input: './src/index.ts',
-  output: {
-    name: 'Waves',
-    format: 'umd',
-    file: isProduction ? 'dist/index.js' : 'node_modules/.waves/dist/index.js',
-    sourcemap: !isProduction,
-    banner: `/*! ${pkg.name} v${pkg.version} | ${new Date().toGMTString()} | ${pkg.homepage} */`,
-  },
+  output: isProduction
+    ? [
+      {
+        ...proOutputOpt,
+        format: 'umd',
+        file: 'dist/index.js',
+      },
+      {
+        ...proOutputOpt,
+        format: 'cjs',
+        file: 'dist/index.cjs.js',
+      },
+      {
+        ...proOutputOpt,
+        format: 'es',
+        file: 'dist/index.esm.js',
+      },
+    ]
+    : {
+      name,
+      format: 'umd',
+      file: 'node_modules/.waves/dist/index.js',
+      sourcemap: true,
+    },
   plugins: [
     ...basePlugins,
     ...(
